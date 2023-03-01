5c6faa8da60b8a378ada474ddf96fe66117d2b5a
pomdtr <achille.lacoin@gmail.com>
2023-03-01 20:54:07 +0100

    add secondary actions for discovery

--- src/search-discovery.tsx
+++ src/search-discovery.tsx
@@ -29,18 +29,30 @@ function Release({ release }: { release: Release }) {
     <List.Item
       title={release.discovery.title}
       subtitle={release.discovery.tagline}
-      icon={release.icon_url ? { source: release.icon_url, mask: Image.Mask.RoundedRectangle } : Icon.Dot}
+      icon={release.icon_url ? { source: release.icon_url, mask: Image.Mask.RoundedRectangle } : Icon.PlusTopRightSquare}
       actions={
         <ActionPanel>
           <ActionPanel.Section>
-            <Action.OpenInBrowser title="Open in Browser" url={release.discovery.listed_url} />
+            <Action.OpenInBrowser title="Open in Discovery" url={release.discovery.listed_url} />
+            {release.discovery.homepage ? <Action.OpenInBrowser icon={Icon.House} title="Open Homepage" url={release.discovery.homepage} /> : null}
+            {release.discovery.git ? <Action.OpenInBrowser icon={Icon.Code} title="Open Git Repository" url={release.discovery.git} shortcut={{ modifiers: ["cmd", "ctrl"], key: "enter" }} /> : null}
           </ActionPanel.Section>
           <ActionPanel.Section>
             <Action.CopyToClipboard
-              title="Copy Url"
+              title="Copy Discovery Link"
               content={`${release.discovery.listed_url}`}
               shortcut={{ modifiers: ["cmd"], key: "." }}
             />
+            {release.discovery.homepage ? <Action.CopyToClipboard
+              title="Copy Homepage Link"
+              content={`${release.discovery.homepage}`}
+              shortcut={{ modifiers: ["cmd", "shift"], key: "." }}
+            /> : null}
+            {release.discovery.git ? <Action.CopyToClipboard
+              title="Copy Git Repository Link"
+              content={`${release.discovery.git}`}
+              shortcut={{ modifiers: ["cmd", "ctrl"], key: "." }}
+            /> : null}
           </ActionPanel.Section>
         </ActionPanel>
       }
