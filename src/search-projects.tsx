f6e346019af97e15757baff1aaa91c436ba1fee2
pomdtr <achille.lacoin@gmail.com>
2023-03-01 16:45:35 +0100

    add search project view
    Co-authored-by: Sofa <SlumberDemon@users.noreply.github.com>

--- src/search-projects.tsx
+++ src/search-projects.tsx
@@ -1,54 +1,56 @@
 // Temp layout, not finished
 
-import { useEffect, useState } from "react";
-import { Action, ActionPanel, List } from "@raycast/api";
+import { Action, ActionPanel, Icon, List } from "@raycast/api";
+import { useSpace } from "./hooks";
 
-const items = ["Palettes"];
+type Project = {
+  id: string;
+  name: string;
+  status: string;
+}
+
+type ProjectResponse = {
+  apps: Project[]
+}
 
 export default function SearchProjects() {
-  const [searchText, setSearchText] = useState("");
-  const [filteredList, filterList] = useState(items);
-
-  useEffect(() => {
-    filterList(items.filter((item) => item.includes(searchText)));
-  }, [searchText]);
+  const { data, isLoading } = useSpace<ProjectResponse>("/apps");
 
   return (
     <List
-      filtering={false}
-      onSearchTextChange={setSearchText}
-      searchBarPlaceholder="Search..."
-      navigationTitle="Builder"
+      isLoading={isLoading}
     >
-      {filteredList.map((item) => (
-        <List.Item
-          key={item}
-          title={item}
-          subtitle="c4dc3643-1980-4e9d-9fcc-1074a86b2139"
-          accessoryTitle="active" // status
-          actions={
-            <ActionPanel>
-              <ActionPanel.Section>
-                <Action.OpenInBrowser title="Open in Browser" url="https://deta.space" />
-              </ActionPanel.Section>
-              <ActionPanel.Section>
-                <Action.CopyToClipboard
-                  title="Copy Url"
-                  content={`https://deta.space`}
-                  shortcut={{ modifiers: ["cmd"], key: "c" }}
-                />
-              </ActionPanel.Section>
-              <ActionPanel.Section>
-                <Action.CopyToClipboard
-                  title="Copy project id"
-                  content={`c4dc3643-1980-4e9d-9fcc-1074a86b2139`}
-                  shortcut={{ modifiers: ["cmd"], key: "i" }}
-                />
-              </ActionPanel.Section>
-            </ActionPanel>
-          }
-        />
+      {data?.apps.map((project) => (
+        <Project key={project.id} project={project} />
       ))}
     </List>
   );
+}
+
+function Project(props: { project: Project }) {
+  const projectUrl = `https://deta.space/builder/${props.project.id}`
+  return <List.Item
+    key={props.project.id}
+    icon={Icon.Hammer}
+    title={props.project.name}
+    actions={
+      <ActionPanel>
+        <ActionPanel.Section>
+          <Action.OpenInBrowser title="Open in Builder" url={projectUrl} />
+        </ActionPanel.Section>
+        <ActionPanel.Section>
+          <Action.CopyToClipboard
+            title="Copy Project Link"
+            content={projectUrl}
+            shortcut={{ modifiers: ["cmd"], key: "c" }}
+          />
+          <Action.CopyToClipboard
+            title="Copy Project ID"
+            content={props.project.id}
+            shortcut={{ modifiers: ["cmd", "shift"], key: "c" }}
+          />
+        </ActionPanel.Section>
+      </ActionPanel>
+    }
+  />
 }
