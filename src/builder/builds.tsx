89b834838d453945d4afc515ba9f36f9e04d7773
Sofa <Nextil3o3@gmail.com>
2023-03-05 17:14:21 +0100

    Moved build to builds.tsx

---
+++ src/builder/builds.tsx
@@ -0,0 +1,46 @@
+import { Color, Icon, List } from "@raycast/api";
+import { useSpace } from "../hooks/use-space";
+
+type Project = {
+    id: string;
+    name: string;
+    status: string;
+}
+
+
+type Build = {
+    id: string;
+    tag: string;
+    app_id: string;
+    status: BuildStatus;
+    created_at: string;
+    updated_at: string;
+}
+
+type BuildStatus = "complete" | "pending" | "internal-error" | "failed"
+
+type BuildsResponse = {
+    builds: Build[]
+}
+
+export default function Build(props: { project: Project }) {
+    const { data } = useSpace<BuildsResponse>(`/builds?app_id=${props.project.id}`)
+
+    const getStatusColor = (status: BuildStatus) => {
+        switch (status) {
+            case "complete":
+                return Color.Green
+            case "failed":
+            case "internal-error":
+                return Color.Red
+            case "pending":
+                return Color.Yellow
+        }
+    }
+
+    return <List.Item.Detail.Metadata>
+        {data?.builds.map((build) =>
+            <List.Item.Detail.Metadata.Label title={build.id} key={build.id} text={{ value: build.status, color: getStatusColor(build.status) }} />
+        )}
+    </List.Item.Detail.Metadata>
+}
--- src/builder/project.tsx
+++ src/builder/project.tsx
@@ -1,5 +1,5 @@
-import { Action, ActionPanel, Color, Icon, List } from "@raycast/api";
-import { useSpace } from "../hooks/use-space";
+import { Action, ActionPanel, Icon, List } from "@raycast/api";
+import Build from "./builds";
 
 type Project = {
     id: string;
@@ -7,20 +7,6 @@ type Project = {
     status: string;
 }
 
-type Build = {
-    id: string;
-    tag: string;
-    app_id: string;
-    status: BuildStatus;
-    created_at: string;
-    updated_at: string;
-}
-
-type BuildStatus = "complete" | "pending" | "internal-error" | "failed"
-
-type BuildsResponse = {
-    builds: Build[]
-}
 
 export default function OpenProject(props: { project: Project, }) {
     // const { data, isLoading } = useSpace
@@ -132,26 +118,4 @@ export default function OpenProject(props: { project: Project, }) {
             </List.Section>
         </List>
     );
-}
-
-function Build(props: { project: Project }) {
-    const { data } = useSpace<BuildsResponse>(`/builds?app_id=${props.project.id}`)
-
-    const getStatusColor = (status: BuildStatus) => {
-        switch (status) {
-            case "complete":
-                return Color.Green
-            case "failed":
-            case "internal-error":
-                return Color.Red
-            case "pending":
-                return Color.Yellow
-        }
-    }
-
-    return <List.Item.Detail.Metadata>
-        {data?.builds.map((build) =>
-            <List.Item.Detail.Metadata.Label title={build.id} key={build.id} text={{ value: build.status, color: getStatusColor(build.status) }} />
-        )}
-    </List.Item.Detail.Metadata>
 }
