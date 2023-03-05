9a54debf12732d86d56588f355a81169425c3f2d
pomdtr <achille.lacoin@gmail.com>
2023-03-05 14:06:52 +0100

    add ability to generate a new key

--- src/search-collections.tsx
+++ src/search-collections.tsx
@@ -1,5 +1,6 @@
-import { Action, ActionPanel, Icon, List } from "@raycast/api";
-import { useSpace } from "./hooks/use-space";
+import { Action, ActionPanel, Form, Icon, List, showToast, Toast, Clipboard, showHUD } from "@raycast/api";
+import { spaceCient, useSpace } from "./hooks/use-space";
+import { useForm, FormValidation } from "@raycast/utils";
 
 type Collection = {
   id: string;
@@ -45,7 +46,45 @@ function CollectionList(props: { collection: Collection }) {
         <ActionPanel.Section>
           <Action.CopyToClipboard title="Copy Link" content={`https://deta.space/collections/${props.collection.id}`} shortcut={{ modifiers: ["cmd"], key: "." }} />
         </ActionPanel.Section>
+        <ActionPanel.Section>
+          <Action.Push icon={Icon.Key} title="Generate Data Key" target={<GenerateDataKeyForm collectionID={props.collection.id} />} />
+        </ActionPanel.Section>
       </ActionPanel>
     }
   />
+}
+
+type KeyFormValue = {
+  name: string;
+}
+
+type CreateKeyResponse = {
+  name: string;
+  created_at: string;
+  value: string;
+}
+
+function GenerateDataKeyForm(props: { collectionID: string }) {
+  const { handleSubmit, itemProps } = useForm<KeyFormValue>({
+    async onSubmit(values) {
+      try {
+        const res = await spaceCient.post<CreateKeyResponse>(`/collections/${props.collectionID}/keys`, values)
+        await Clipboard.copy(res.value)
+        await showHUD(`Key ${values.name} generated and copied to clipboard`)
+      } catch (err) {
+        showToast(Toast.Style.Failure, "Failed to generate key", (err as Error).message)
+      }
+    },
+    validation: {
+      name: FormValidation.Required,
+    }
+  });
+
+  return <Form actions={
+    <ActionPanel>
+      <Action.SubmitForm title="Generate Key" shortcut={{ modifiers: ["cmd", "shift"], key: "k" }} onSubmit={handleSubmit} />
+    </ActionPanel>
+  }>
+    <Form.TextField title="Name" placeholder="key name" {...itemProps.name} />
+  </Form>
 }
