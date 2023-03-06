2647c2603fa0725d63bb32bd4ea0efc8f83f4711
pomdtr <achille.lacoin@gmail.com>
2023-03-06 20:33:37 +0100

    add number of installs to discovery

--- src/search-discovery.tsx
+++ src/search-discovery.tsx
@@ -2,19 +2,12 @@ import { ActionPanel, Action, List, Icon, Image } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 
 export default function SearchDiscovery() {
-  const { data, isLoading } = useFetch(
-    "https://deta.space/api/v0/discovery/apps",
-    {
-      parseResponse: parseFetchResponse,
-    }
-  );
+  const { data, isLoading } = useFetch("https://deta.space/api/v0/discovery/apps", {
+    parseResponse: parseFetchResponse,
+  });
 
   return (
-    <List
-      isLoading={isLoading}
-      navigationTitle="Discovery"
-      searchBarPlaceholder="Search..."
-    >
+    <List isLoading={isLoading} navigationTitle="Discovery" searchBarPlaceholder="Search...">
       <List.Section title="Results" subtitle={data?.length + ""}>
         {data?.map((release) => (
           <Release key={release.app_id} release={release} />
@@ -27,15 +20,27 @@ export default function SearchDiscovery() {
 function Release({ release }: { release: Release }) {
   return (
     <List.Item
-      title={release.discovery.title}
+      icon={
+        release.icon_url ? { source: release.icon_url, mask: Image.Mask.RoundedRectangle } : Icon.PlusTopRightSquare
+      }
+      title={release.discovery.title || release.app_name}
       subtitle={release.discovery.tagline}
-      icon={release.icon_url ? { source: release.icon_url, mask: Image.Mask.RoundedRectangle } : Icon.PlusTopRightSquare}
+      accessories={[{ tag: { value: `${release.discovery.stats.total_installs} installs` } }]}
       actions={
         <ActionPanel>
           <ActionPanel.Section>
             <Action.OpenInBrowser title="Open in Discovery" url={release.discovery.listed_url} />
-            {release.discovery.homepage ? <Action.OpenInBrowser icon={Icon.House} title="Open Homepage" url={release.discovery.homepage} /> : null}
-            {release.discovery.git ? <Action.OpenInBrowser icon={Icon.Code} title="Open Git Repository" url={release.discovery.git} shortcut={{ modifiers: ["cmd", "ctrl"], key: "enter" }} /> : null}
+            {release.discovery.homepage ? (
+              <Action.OpenInBrowser icon={Icon.House} title="Open Homepage" url={release.discovery.homepage} />
+            ) : null}
+            {release.discovery.git ? (
+              <Action.OpenInBrowser
+                icon={Icon.Code}
+                title="Open Git Repository"
+                url={release.discovery.git}
+                shortcut={{ modifiers: ["cmd", "ctrl"], key: "enter" }}
+              />
+            ) : null}
           </ActionPanel.Section>
           <ActionPanel.Section>
             <Action.CopyToClipboard
@@ -43,16 +48,20 @@ function Release({ release }: { release: Release }) {
               content={`${release.discovery.listed_url}`}
               shortcut={{ modifiers: ["cmd"], key: "." }}
             />
-            {release.discovery.homepage ? <Action.CopyToClipboard
-              title="Copy Homepage Link"
-              content={`${release.discovery.homepage}`}
-              shortcut={{ modifiers: ["cmd", "shift"], key: "." }}
-            /> : null}
-            {release.discovery.git ? <Action.CopyToClipboard
-              title="Copy Git Repository Link"
-              content={`${release.discovery.git}`}
-              shortcut={{ modifiers: ["cmd", "ctrl"], key: "." }}
-            /> : null}
+            {release.discovery.homepage ? (
+              <Action.CopyToClipboard
+                title="Copy Homepage Link"
+                content={`${release.discovery.homepage}`}
+                shortcut={{ modifiers: ["cmd", "shift"], key: "." }}
+              />
+            ) : null}
+            {release.discovery.git ? (
+              <Action.CopyToClipboard
+                title="Copy Git Repository Link"
+                content={`${release.discovery.git}`}
+                shortcut={{ modifiers: ["cmd", "ctrl"], key: "." }}
+              />
+            ) : null}
           </ActionPanel.Section>
         </ActionPanel>
       }
@@ -62,10 +71,11 @@ function Release({ release }: { release: Release }) {
 
 type Release = {
   app_id: string;
+  app_name: string;
   version: string;
   icon_url?: string;
   discovery: {
-    title: string;
+    title?: string;
     tagline: string;
     theme_color: string;
     git?: string;
@@ -73,20 +83,20 @@ type Release = {
     listed_url: string;
     stats: {
       total_installs: number;
-    }
-  }
-}
+    };
+  };
+};
 
 type DiscoveryResponse = {
-  releases: Release[]
-}
+  releases: Release[];
+};
 
 async function parseFetchResponse(response: Response): Promise<Release[]> {
   if (!response.ok) {
     throw new Error(response.statusText);
   }
 
-  const payload = await response.json() as DiscoveryResponse;
+  const payload = (await response.json()) as DiscoveryResponse;
 
-  return payload.releases.filter((release) => release.discovery.title != "").sort((a, b) => b.discovery.stats.total_installs - a.discovery.stats.total_installs);
+  return payload.releases.sort((a, b) => b.discovery.stats.total_installs - a.discovery.stats.total_installs);
 }
