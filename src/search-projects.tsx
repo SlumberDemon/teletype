647b07169c2547df53b4bed73a91e2d93abab99d
pomdtr <achille.lacoin@gmail.com>
2023-03-19 11:58:40 +0100

    add ability to open the builder instance from the project view

--- src/search-projects.tsx
+++ src/search-projects.tsx
@@ -3,25 +3,42 @@ import { useSpace } from "./hooks/use-space";
 import RevisionList from "./builder/revisions";
 import Release from "./builder/releases";
 import BuildList from "./builder/builds";
-import { Project } from "./types/types";
+import { Project, Instance } from "./types/types";
+import { useMemo } from "react";
 
 type ProjectResponse = {
   apps: Project[];
 };
 
+type InstancesResponse = {
+  instances: Instance[];
+};
+
 export default function SearchProjects() {
-  const { data, isLoading } = useSpace<ProjectResponse>("/apps");
+  const { data: projects, isLoading: projectsLoading } = useSpace<ProjectResponse>("/apps");
+  const { data: instances, isLoading: instancesLoading } = useSpace<InstancesResponse>(
+    "/instances?channel=development"
+  );
+
+  const instanceMap = useMemo(() => {
+    return instances?.instances.reduce((acc, instance) => {
+      acc[instance.app_id] = instance;
+      return acc;
+    }, {} as Record<string, Instance>);
+  }, [instances]);
+
+  console.log(instanceMap);
 
   return (
-    <List isLoading={isLoading} navigationTitle="Builder">
-      {data?.apps.map((project) => (
-        <Project key={project.id} project={project} />
+    <List isLoading={projectsLoading && instancesLoading} navigationTitle="Builder">
+      {projects?.apps.map((project) => (
+        <Project key={project.id} project={project} instance={instanceMap?.[project.id]} />
       ))}
     </List>
   );
 }
 
-function Project(props: { project: Project }) {
+function Project(props: { project: Project; instance?: Instance }) {
   return (
     <List.Item
       key={props.project.id}
@@ -36,11 +53,27 @@ function Project(props: { project: Project }) {
         <ActionPanel>
           <ActionPanel.Section>
             <Action.OpenInBrowser title="Open in Builder" url={`https://deta.space/builder/${props.project.id}`} />
+            {props.instance ? <Action.OpenInBrowser title="Open Builder Instance" url={props.instance.url} /> : null}
           </ActionPanel.Section>
           <ActionPanel.Section>
-            <Action.Push icon={Icon.Hammer} title="View Builds" target={<BuildList project={props.project} />} shortcut={{ modifiers: ["cmd"], key: "b" }} />
-            <Action.Push icon={Icon.List} title="View Revisions" target={<RevisionList project={props.project} />} shortcut={{ modifiers: ["cmd"], key: "r" }} />
-            <Action.Push icon={Icon.Globe} title="View Releases" target={<Release project={props.project} />} shortcut={{ modifiers: ["cmd", "shift"], key: "r" }} />
+            <Action.Push
+              icon={Icon.Hammer}
+              title="View Builds"
+              target={<BuildList project={props.project} />}
+              shortcut={{ modifiers: ["cmd"], key: "b" }}
+            />
+            <Action.Push
+              icon={Icon.List}
+              title="View Revisions"
+              target={<RevisionList project={props.project} />}
+              shortcut={{ modifiers: ["cmd"], key: "r" }}
+            />
+            <Action.Push
+              icon={Icon.Globe}
+              title="View Releases"
+              target={<Release project={props.project} />}
+              shortcut={{ modifiers: ["cmd", "shift"], key: "r" }}
+            />
           </ActionPanel.Section>
           <ActionPanel.Section>
             <Action.CopyToClipboard
